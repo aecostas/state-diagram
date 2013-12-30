9a76bc21c9a0deda3965fe6c828b50b81a33249f
Andres Estevez <andres.estevez@quobis.com>
2013-12-30 16:48:36 +0100

    Added version

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Node style
-#   solid
-#   dashed
-#   dotted
-#   bold
-#   rounded
-#   diagonals
-#   filled
-#   striped
-#   wedged
-#
+# Author: Andres Estevez Costas <aecostas@gmail.com>
+# Date: 12/30/2013
 
 import re
 import os
@@ -53,7 +56,7 @@ def main(argv):
                 comment_tmp = fields[1].split(',')
             except IndexError:
                 comment_tmp = ''
-                
+
             arrow = edge[0]
             try:
                 arrow_color = edge[1]
